7f17776e25ebc9ad3569df2e6cd4e031c3e03c95
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-27 18:30:04 +0200

    fix lib exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
 export { validate, validate as validateForm } from './validateForm'
 export { validateInput } from './validateInput'
 export { capitalize } from './capitalize'
+export { handleError } from './handleError'
