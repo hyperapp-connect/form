0e6425211331702c8a73e46b8c5240fc8473cb67
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-27 18:29:58 +0200

    export lib

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ export { ErrorMsg } from './ErrorMsg'
 export { Form } from './Form'
 export { Input } from './Input'
 
-export { validateForm, validateInput } from './lib'
+export * from './lib'
 
 export default Form
